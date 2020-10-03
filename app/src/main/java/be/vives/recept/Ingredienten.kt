9ec62ffc768d4e552cc826647cd3f50f0468103e
mattvancoillie <60688811+mattvancoillie@users.noreply.github.com>
2020-10-03 11:47:10 +0200

    add classes

---
+++ app/src/main/java/be/vives/recept/Ingredienten.kt
@@ -0,0 +1,4 @@
+package be.vives.recept
+
+class Ingredienten {
+}
--- app/src/main/java/be/vives/recept/MainActivity.kt
+++ app/src/main/java/be/vives/recept/MainActivity.kt
@@ -2,8 +2,16 @@ package be.vives.recept
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import be.vives.recept.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val benodigdheden: Benodigdheden = Benodigdheden()
+    private val ingredienten: Ingredienten = Ingredienten()
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
